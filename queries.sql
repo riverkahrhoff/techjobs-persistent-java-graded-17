d3df863ad7039f9d0e9b7893efa263988a1e201a
riverkahrhoff <river.kahrhoff@gmail.com>
2024-11-15 18:30:23 -0600

    TestTaskThree passing

--- queries.sql
+++ queries.sql
@@ -6,5 +6,6 @@
 --Part 2
 SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
+DROP TABLE job;
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +22,12 @@ import java.util.Optional;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
     @RequestMapping("/")
     public String index(Model model) {
 
@@ -28,6 +39,13 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
 	model.addAttribute("title", "Add Job");
+
+        List<Employer> employers = (List<Employer>) employerRepository.findAll();
+        model.addAttribute("employers", employers);
+
+        List<Skill> skills = (List<Skill>) skillRepository.findAll();
+        model.addAttribute("skills", skills);
+
         model.addAttribute(new Job());
         return "add";
     }
@@ -40,6 +58,11 @@ public class HomeController {
 	    model.addAttribute("title", "Add Job");
             return "add";
         }
+        Optional<Employer> employer = employerRepository.findById(employerId);
+        if (employer.isPresent()) {
+            newJob.setEmployer(employer.get());
+        }
+
 
         return "redirect:";
     }
